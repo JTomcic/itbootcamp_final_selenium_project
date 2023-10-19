8f30627070600872ce6ccf435028cbcc196c5e6a
Jovana Tomcic <tomcicjovana92@gmail.com>
2023-10-19 21:07:36 +0200

    Add Visits the admin cities page Test

--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -12,4 +12,32 @@ public class AdminCitiesTests extends BasicTest {
                 baseUrl,
                 "Should be redirected to 'https://vue-demo.daniel-avellaneda.com/'.");
     }
+    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void visitsTheAdminCitiesPage() {
+        String adminEmail = "admin@admin.com";
+        String adminPassword = "12345";
+
+        navPage.clickOnNavLoginLink();
+        navPage.waitForRedirectionToLoginPage();
+
+        loginPage.clearAndTypeEmail(adminEmail);
+        loginPage.clearAndTypePassword(adminPassword);
+        loginPage.clickOnLoginButton();
+        loginPage.waitForRedirectionToHomePage();
+        Assert.assertEquals(
+                driver.getCurrentUrl(),
+                baseUrl + "home",
+                "URL should still be 'https://vue-demo.daniel-avellaneda.com/home'.");
+
+        loginPage.clickOnAdminLink();
+        loginPage.waitForVisibilityOfAdminMenu();
+
+        loginPage.clickOnCitiesButtonFromAdminList();
+        loginPage.waitForRedirectionToAdminCitiesPage();
+
+        Assert.assertEquals(
+                driver.getCurrentUrl(),
+                baseUrl + "admin/cities",
+                "Should be redirected to Admin Cities page.");
+    }
 }
